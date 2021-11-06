2cd35fbe2de8d3a5161e292d445e88fbc6cad4c6
DaCuteRaccoon <72467156+DaCuteRaccoon@users.noreply.github.com>
2021-11-06 02:59:46 -0700

    Delete wonky-text.js

--- src/wonky-text.js
+++
@@ -1,7 +0,0 @@
-(function() {
-	var x = document.body.getElementsByTagName("*");
-	for(i = 0; i < x.length; i++) {
-		var y = Math.floor((Math.random() * 10) + 1);
-		x[i].style.transform = "rotate(" + y + "deg)"
-	}
-}());
